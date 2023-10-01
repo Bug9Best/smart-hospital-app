35c91ef9bc67058b39f965cc8e9eb7cb14c76c1f
Phuri Siripool <85283698+Phuriq@users.noreply.github.com>
2023-10-01 20:29:26 +0700

    FiX-ERROR

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import base from './src/modules/base_module';
-import { DoctorService } from './src/services/doctor/doctor_service';
-import { UserService } from './src/services/user/user_service';
 
 export default function App() {
-  const userService = new UserService();
-  userService.create();
 
   return (
     <base.View style={styles.container}>
-      <base.Text>Best</base.Text>
+      
       <base.TouchableOpacity>
       </base.TouchableOpacity>
     </base.View>
@@ -18,7 +14,7 @@ export default function App() {
 const styles = base.StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
