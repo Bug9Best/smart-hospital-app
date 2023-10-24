98fb2ac3909af3862d16687f6241d3aba7225b65
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-24 15:23:47 +0700

    updpate base service

--- src/services/base/base_service.js
+++ src/services/base/base_service.js
@@ -1,8 +1,7 @@
 import { environment } from '../../environment/environment'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+var HTTPService = require('react-native-http').HTTPService;
 
-class BaseService {
+class BaseService extends HTTPService {
   baseUrl = environment.BaseURL;
   path = '_blank';
 
@@ -14,74 +13,24 @@ class BaseService {
     return this.baseUrl + this.path;
   }
 
-  create(data) {
-    return this.axios.post(this.BaseURL, data)
-      .then((response) => {
-        this.item = response.data;
-        this.item$.next(this.item);
-        return response.data;
-      })
-      .catch((error) => {
-        console.error('Error creating item:', error);
-        throw error;
-      })
-      .finally(() => {
-        this.loading = false;
-        this.loading$.next(this.loading);
-      });
-  }
-
-  get(options) {
-    return console.log(this.BaseURL);
-    return this.client.get(this.baseUrl, {
-      params: this.options('get', options)
-    })
-      .then((response) => {
-        this.items = response.data;
-        this.items$.next(this.items);
-        return response.data;
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        throw error;
-      });
+  get(_options) {
+    return this.get(this.BaseURL);
   }
 
   show(id) {
-    return this.client.get(this.baseUrl + '/' + id.toString())
-      .then((response) => {
-        this.item = response.data;
-        this.item$.next(this.item);
-        return response.data;
-      })
-      .catch((error) => {
-        console.error('Error fetching item:', error);
-        throw error;
-      });
+    return this.get(this.BaseURL + '/' + id.toString());
   }
 
-  update(item) {
-    if (item.id) {
-      return this.client.put(this.baseUrl + '/' + item.id.toString(), item)
-        .then((response) => {
-          this.item = response.data;
-          this.item$.next(this.item);
-          return response.data;
-        })
-        .catch((error) => {
-          console.error('Error updating item:', error);
-          throw error;
-        });
-    } else {
-      console.error(`[${this.path}] Required model with [id]`);
-      return new Promise((resolve) => {
-        resolve(null);
-      });
-    }
+  create(data) {
+    return this.post(this.BaseURL, data);
+  }
+
+  update(data) {
+    return this.put(this.BaseURL + '/' + data.id.toString(), data);
   }
 
   delete(id) {
-    return this.remove(id);
+    return this.delete(this.BaseURL + '/' + id.toString());
   }
 }
 
