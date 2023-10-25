0c96d959bfe67a5d0189e729f52b087f7083a81f
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-26 03:47:03 +0700

    update about us

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -4,39 +4,33 @@ import Layer from "../Layout/lgradient";
 
 export default function Aboutus({ navigation }) {
   return (
-    
+
     <base.View style={styles.container}>
       <base.ScrollView>
+        <base.View style={styles.box}>
+          <base.Image
+            source={{ uri: 'http://medicalcenter.kmitl.ac.th/wp-content/uploads/2019/05/93748.jpg' }}
+            style={{ width: "100%", height: 400 }} />
+          <base.Text style={{ marginTop: 10, fontSize: 20, fontWeight: 800, padding: 10 }}>
+            ความเป็นมา
+          </base.Text>
+          <base.Text style={{ fontSize: 15, padding: 10 }}>
+            คลินิกเวชกรรม สจล. เดิมที เป็นหน่วยงานสุขภาพอนามัย บริหารภายใต้สังกัด
+            กองกิจการนักศึกษา สำนักงานอธิการบดี สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณ
+            ทหารลาดกระบัง ภายหลังเปลี่ยนเป็น คลินิกเวชกรรม สจล. บริหารภายใต้สังกัด
+            คณะแพทยศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
+          </base.Text>
+          <base.Image
+            source={{ uri: 'http://medicalcenter.kmitl.ac.th/wp-content/uploads/2019/05/e0b982e0b884e0b8a3e0b887e0b881e0b8b2e0b8a3e0b888e0b8b4e0b895e0b8ade0b8b2e0b8aae0b8b2170261_e0b991e0b998e0b990e0b997e0b990e0b992_0109.jpg' }}
+            style={{ marginTop: 10, width: "100%", height: 500 }}
+          />
+          <base.Text style={{ marginTop: 15, fontSize: 15, fontWeight: 400, padding: 10 }}>
+            โดย การให้บริการของคลินิกเวชกรรม สจล. นี้ จัดขึ้นเพื่อดูแล ป้องกัน รักษา และแนะนำเกี่ยวกับการ
+            ดูแลสุขภาพอนามัยของตนเอง และชุมชน ซึ่งเป็นสิ่งจำเป็นเพื่อให้มีสุขภาพแข็งแรง สมบูรณ์ ทั้งทางร่างกาย จิตใจ อารมณ์และสังคม
+            เพื่อให้ปราศจากโรคภัยไข้เจ็บ ที่จะบั่นทอนความสามารถในการดำรงชีวิต
+          </base.Text>
 
-      <base.View style={{ padding: 5 }}>
-        <base.Text style={{ color: "#FF8A48", fontSize: 20, fontWeight: 800 }}>
-          ความเป็นมา
-        </base.Text>
-      </base.View>
-      
-      <base.View style={styles.box}>
-        <base.Image
-          source={require("../assets/image/Haerin.png")}
-          style={{width: "100%", height: 500, marginTop: 10}}
-        />
-        <base.Text style={{marginTop: 10, fontSize: 15, fontWeight: 400, padding: 10 }}>
-          คลินิกเวชกรรม สจล. เดิมที เป็นหน่วยงานสุขภาพอนามัย บริหารภายใต้สังกัด
-          กองกิจการนักศึกษา สำนักงานอธิการบดี สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณ
-          ทหารลาดกระบัง ภายหลังเปลี่ยนเป็น คลินิกเวชกรรม สจล. บริหารภายใต้สังกัด
-          คณะแพทยศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
-        </base.Text>
-        <base.Image
-          source={require("../assets/image/Haerin.png")}
-          style={{marginTop: 10, width: "100%", height: 500}}
-        />
-        <base.Text style={{marginTop: 15, fontSize: 15, fontWeight: 400, padding: 10}}>
-          คลินิกเวชกรรม สจล. เดิมที เป็นหน่วยงานสุขภาพอนามัย บริหารภายใต้สังกัด
-          กองกิจการนักศึกษา สำนักงานอธิการบดี สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณ
-          ทหารลาดกระบัง ภายหลังเปลี่ยนเป็น คลินิกเวชกรรม สจล. บริหารภายใต้สังกัด
-          คณะแพทยศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
-        </base.Text>
-
-      </base.View>
+        </base.View>
 
       </base.ScrollView>
     </base.View>
@@ -52,15 +46,13 @@ const styles = base.StyleSheet.create({
     padding: 16,
   },
   box: {
-    // flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
-    marginTop: 15,
     borderRadius: 10,
     padding: 16,
     marginLeft: 5,
     marginRight: 5
-    
+
   },
 });
