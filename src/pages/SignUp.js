86ff344bb0ab1ca00d6ec35ef734e1d69ab4ceef
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-26 06:30:47 +0700

    update

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,46 +2,46 @@ import base from '../modules/base_module'
 import Layer from "../Layout/lgradient";
 
 
-export default function SignUp({ navigation }){
-    return(
+export default function SignUp({ navigation }) {
+  return (
     <base.View style={styles.container}>
-<base.View style={styles.box}>
-        <base.Text style={{color: "#FF8A48" ,fontSize: 15, fontWeight: 800}}>
-            ข้อตกลงและเงื่อนไข
+      <base.View style={styles.box}>
+        <base.Text style={{ color: "#FF8A48", fontSize: 15, fontWeight: 800 }}>
+          ข้อตกลงและเงื่อนไข
         </base.Text>
-        <base.Text style={{marginTop: 20, fontWeight: 600}} >
-        1. ลงทะเบียนได้เฉพาะผู้ที่มีบัตรประจำตัวประชาชน 
-(Thai National ID Card) และยังไม่ถึงวันหมดอายุ
+        <base.Text style={{ marginTop: 20, fontWeight: 600 }} >
+          1. ลงทะเบียนได้เฉพาะผู้ที่มีบัตรประจำตัวประชาชน
+          (Thai National ID Card) และยังไม่ถึงวันหมดอายุ
         </base.Text>
-        <base.Text style={{marginTop: 20, fontWeight: 600}}>
-        2. ผู้ลงทะเบียนขอรับรองว่าข้อมูลที่จะให้กับคลินิกเวชกรรม สจล. ทั้งหมดนี้ถูกต้องตรงกับความจริงทุกประการและยินยอมให้ทาง
-คลินิกเวชกรรม สจล. ตรวจสอบจากฐานข้อมูลทางทะเบียนใดๆ ของรัฐฯ รวมถึงอนุญาตให้ใช้รูปภาพของข้าพเจ้าเพื่อการมีเวชระเบียนและการตรวจ
-รักษา หากมีข้อมูลใดไม่ถูกต้องหรือไม่ตรงกับความจริงและอาจจะทำให้
-เกิดความเสียหายแก่ตัวข้าพเจ้าหรือบุคคลอื่น ข้าพเจ้ายินยอมรับผิดชอบ ในความเสียหายที่เกิดขึ้นทุกประการ
+        <base.Text style={{ marginTop: 20, fontWeight: 600 }}>
+          2. ผู้ลงทะเบียนขอรับรองว่าข้อมูลที่จะให้กับคลินิกเวชกรรม สจล. ทั้งหมดนี้ถูกต้องตรงกับความจริงทุกประการและยินยอมให้ทาง
+          คลินิกเวชกรรม สจล. ตรวจสอบจากฐานข้อมูลทางทะเบียนใดๆ ของรัฐฯ รวมถึงอนุญาตให้ใช้รูปภาพของข้าพเจ้าเพื่อการมีเวชระเบียนและการตรวจ
+          รักษา หากมีข้อมูลใดไม่ถูกต้องหรือไม่ตรงกับความจริงและอาจจะทำให้
+          เกิดความเสียหายแก่ตัวข้าพเจ้าหรือบุคคลอื่น ข้าพเจ้ายินยอมรับผิดชอบ ในความเสียหายที่เกิดขึ้นทุกประการ
         </base.Text>
-        <base.Text style={{marginTop: 20, fontWeight: 600}}>
-        3. ผู้ลงทะเบียนยินยอมให้ใช้ข้อมูลประวัติการรักษาของผู้ลงทะเบียนไป
-ใช้เพื่อการศึกษา การรักษาพยาบาล การวิจัย การเรียนการสอน และการ
-พัฒนาคุณภาพโรงพยาบาลฯ
+        <base.Text style={{ marginTop: 20, fontWeight: 600 }}>
+          3. ผู้ลงทะเบียนยินยอมให้ใช้ข้อมูลประวัติการรักษาของผู้ลงทะเบียนไป
+          ใช้เพื่อการศึกษา การรักษาพยาบาล การวิจัย การเรียนการสอน และการ
+          พัฒนาคุณภาพโรงพยาบาลฯ
         </base.Text>
-        <base.Text style={{marginTop: 20, fontWeight: 600}}>
-        4. กรณีผู้ลงทะเบียนไม่เคยมีบัตรประจำตัวประชาชน หรือบุคคลต่างชาติ-
-ต่างด้าว ขอให้มาติดต่อลงทะเบียนที่เวชระเบียน คลินิกเวชกรรม สจล.
+        <base.Text style={{ marginTop: 20, fontWeight: 600 }}>
+          4. กรณีผู้ลงทะเบียนไม่เคยมีบัตรประจำตัวประชาชน หรือบุคคลต่างชาติ-
+          ต่างด้าว ขอให้มาติดต่อลงทะเบียนที่เวชระเบียน คลินิกเวชกรรม สจล.
         </base.Text>
       </base.View>
       <base.View>
-        <base.TouchableOpacity onPress={()=>{navigation.navigate('Signupjing', {text: "text"})}}>
+        <base.TouchableOpacity onPress={() => { navigation.navigate('Signupjing', { text: "text" }) }}>
           <base.View
             style={{
-                paddingHorizontal: 125,
-                paddingVertical: 15,
-                marginTop: 200,
-                backgroundColor: "#FF8A48",
-                borderRadius: 10,
+              paddingHorizontal: 125,
+              paddingVertical: 15,
+              marginTop: 200,
+              backgroundColor: "#FF8A48",
+              borderRadius: 10,
             }}
           >
-            <base.Text style={{color: "white"}} >
-            ดำเนินการต่อ
+            <base.Text style={{ color: "white" }} >
+              ดำเนินการต่อ
             </base.Text>
           </base.View>
         </base.TouchableOpacity>
@@ -62,7 +62,7 @@ const styles = base.StyleSheet.create({
     marginTop: 15,
     borderRadius: 10,
     padding: 16,
-    
+
   },
   Realinput: {
     borderWidth: 2,
